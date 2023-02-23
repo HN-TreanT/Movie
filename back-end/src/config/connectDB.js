9106fa25fdb19e071a0fffce842bf2371f9d3f14
HN-TreanT <hntreant232gmail.com>
2023-02-23 22:28:11 +0700

    env example

--- back-end/src/config/connectDB.js
+++ back-end/src/config/connectDB.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 async function connect() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/movie");
+    await mongoose.connect("mongodb://localhost:27017/webfilm");
     console.log("connect succeeded");
   } catch (e) {
     console.log("connect fail");
